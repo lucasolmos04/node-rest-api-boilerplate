2342753b24a5503a1df31ae3e4f88b580fdbabf1
Lucas Olmos <lucolmos@gmail.com>
2019-10-23 21:11:24 -0300

    create controller

--- src/application/controllers/users.controller.js
+++ src/application/controllers/users.controller.js
@@ -1,9 +1,29 @@
-const infrastructure = require('../../infrastucture/index.infrastucture');
+const fetch = require('node-fetch');
+const moment = require('moment');
+
+const endpoint = 'https://2eja2nqth0.execute-api.us-east-1.amazonaws.com/api/users';
+const method = {method: 'get'};
+
+const getUsers = () => {
+    return fetch(endpoint, method)
+    .then(response => response.json())
+    .catch((err) =>{
+        console.log('Error:' , err);
+        logOut.error(` - La data tuvo un error`);
+    })
+}
 
 class UsersCtrl  {
     async getUsers(req, res){
-        const users = await infrastructure.getUsers();
-        console.log(users);
+        const users = await getUsers();
+       
+        for(let i = 0; i < users.users.length; i ++){
+            let user = users.users[i];
+            if(user.is_active){
+               user.age = moment().diff(user.birthday, 'years');
+            }
+        }
+        res.status(200).json(users);
     }
 }
 
