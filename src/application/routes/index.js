3c8681ddf05ebe005a8e85ecf43c34258e559aaa
Lucas Olmos <lucolmos@gmail.com>
2019-10-23 21:12:31 -0300

    add route in router

--- src/application/routes/index.js
+++ src/application/routes/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = new express.Router();
 
 const HelloWorldCtrl = new (require('../controllers/helloWorld.controller'))();
-const usersController = require('../controllers/users.controller');
+const usersController = new (require('../controllers/users.controller'))();
 
 router
 	.route('/hello-world')
 	.get(HelloWorldCtrl.sayHi);
+
+router
+    .route('/users')
+    .get(usersController.getUsers);
 
 module.exports = router;
